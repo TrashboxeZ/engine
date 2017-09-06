7774cbd930b637cae83a86f35d752f4d3bd24531
web <web@training.workrocks.ua>
2017-09-06 20:56:43 +0300

    mapper

--- vendor/engine/Controller.php
+++ vendor/engine/Controller.php
@@ -3,13 +3,17 @@
 namespace Engine;
 
 use Engine\View;
+use Engine\Request;
 
 class Controller
 {
     public $view;
+    protected $request;
     
     public function __construct()
     {
         $this->view = new View();
+        $this->request = new Request;
     }
+    
 }
